4f2b0a5aa24d5dc795b5270ef933bfc41d2cbfde
DmitriyIL <idmitriy.mgdn@gmail.com>
2018-12-18 23:49:52 +0300

    YAT the smartest! :)

--- src/main/kotlin/ru/spbstu/competition/Main.kt
+++ src/main/kotlin/ru/spbstu/competition/Main.kt
@@ -26,7 +26,7 @@ fun main(args: Array<String>) {
     val protocol = Protocol(Arguments.url, Arguments.port)
     val graph = Graph()
     val intellect = Intellect(graph, protocol)
-    protocol.handShake("BL-YAT")
+    protocol.handShake("YAT")
     val setupData = protocol.setup()
     graph.init(setupData)
     println("Sites: ${graph.getAllSites().size}")
